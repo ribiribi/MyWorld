5761447f6829cabca81062912cc890fbb20e2144
user143594 <user143594@37-128-186-164.rdns.melbourne.co.uk>
2018-11-12 09:55:45 +0000

    estructura superior acabada

---
+++ MyWorld/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  MyWorld
+//
+//  Created by user143594 on 11/11/18.
+//  Copyright © 2018 user143594. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Navigation
+    
+}
