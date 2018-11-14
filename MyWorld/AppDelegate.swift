d3acba56d70ce2c0524531a6cf3d04ab1414f5bd
user143594 <user143594@37-128-186-164.rdns.melbourne.co.uk>
2018-11-14 11:03:54 +0000

    amb dades de fitxer

--- MyWorld/AppDelegate.swift
+++ MyWorld/AppDelegate.swift
@@ -14,10 +14,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application:UIApplication, didFinishLaunchingWithOptions launchOptions:[UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let manager = PlaceManager.shared
-        for place in manager.someTestPlaces {
-            manager.append(place)
+        
+//        if let jsonData = manager.jsonFrom(places: manager.someTestPlaces){
+//            let docsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+//            let filePath = docsPath.appendingPathComponent("places.json")
+//            do{
+//                try jsonData.write(to: filePath)
+//                print ("Fitxers guardats en local")
+//            } catch {
+//                print ("Error al escriure fitxer local")
+//            }
+//        }
+        
+        let docsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let filePath = docsPath.appendingPathComponent("places.json")
+        do{
+            let jasonData = try Data(contentsOf: filePath)
+            let places = manager.placesFrom(jsonData: jasonData)
+            
+            for place in places{
+                manager.append(place)
+            }
+        } catch {
+            
         }
+        
+//        let manager = PlaceManager.shared
+//        for place in manager.someTestPlaces {
+//            manager.append(place)
+//        }
         return true
     }
-    
 }
