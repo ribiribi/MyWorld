68e0936d7ec626a468165fe5e0b7f912e230de8c
user143594 <user143594@37-128-186-164.rdns.melbourne.co.uk>
2018-12-11 07:33:01 +0000

    animació inicial finalitzada

--- MyWorld/InitialVC.swift
+++ MyWorld/InitialVC.swift
@@ -15,31 +15,41 @@ class InitialVC: UIViewController {
     @IBOutlet weak var imageInitialVC: UIImageView!
     @IBOutlet weak var blureView: UIVisualEffectView!
     @IBOutlet weak var welcomeLAbel: UILabel!
+    @IBOutlet weak var myWorldLabel: UILabel!
+    @IBOutlet weak var worldLabel: UILabel!
     
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
 
-        //self.welcomeLAbel.font = UIFont.boldSystemFont(ofSize:45)
-
-        //let timer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false) { timer in
-            //self.blureView.isHidden = false
-//        UIView.transition(with: view, duration: 2, options: .transitionCrossDissolve, animations: {
-//            //
-//            self.blureView.isHidden = false
-//            self.welcomeLAbel.isHidden = false
-//        })
+        self.welcomeLAbel.font = UIFont (name: "Chalkduster", size: 30)
+        self.myWorldLabel.font = UIFont (name: "Chalkduster", size: 72)
+         self.worldLabel.font = UIFont (name: "Chalkduster", size: 78)
+        
         
             
-        let timer = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false) { timer in
+        Timer.scheduledTimer(withTimeInterval: 1.5, repeats: false) { timer in
             
-            UIView.animate(withDuration: 3, delay: 0.0, options: UIView.AnimationOptions.curveEaseOut, animations: {
+            UIView.animate(withDuration: 2, delay: 0.0, options: UIView.AnimationOptions.curveEaseOut, animations: {
                 self.welcomeLAbel.alpha = 1
-                self.blureView.alpha = 0.8
                 
             }, completion: nil)
+            
+            Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { timer in
+                
+                UIView.animate(withDuration: 2, delay: 0.0, options: UIView.AnimationOptions.curveEaseOut, animations: {
+                    self.myWorldLabel.alpha = 1
+                    self.worldLabel.alpha = 1
+                    self.blureView.alpha = 0.95
+                    
+                }, completion: nil)
+            }
         }
+        
+        
+        
+
 
         
     }
